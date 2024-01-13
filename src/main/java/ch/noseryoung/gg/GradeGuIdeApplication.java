3884c7c50d9fb9feaf88c4f27619445eb1d18631
BambusTM <81088539+BambusTM@users.noreply.github.com>
2024-01-13 16:04:51 +0100

    push springboot framework

---
+++ src/main/java/ch/noseryoung/gg/GradeGuIdeApplication.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.gg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeGuIdeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradeGuIdeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/noseryoung/gg/GradeGuIdeApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.gg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeGuIdeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
