8cdcbd533066c36ead3b485d52c4bca34f556f74
BambusTM <81088539+BambusTM@users.noreply.github.com>
2024-01-31 07:33:56 +0100

    security setup with errors

--- src/main/java/ch/noseryoung/gg/auth/AuthController.java
+++ src/main/java/ch/noseryoung/gg/auth/AuthController.java
@@ -1,16 +1,11 @@
 package ch.noseryoung.gg.auth;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
-import java.security.Principal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,50 +14,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 @Tag(name = "Authentication")
-public class AuthenticationController {
+public class AuthController {
 
-  private final AuthenticationService authenticationService;
+  private final AuthService authService;
 
   @PostMapping("/register")
-  public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
-    String jwtToken = authenticationService.register(request).getAccessToken();
+  public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
+    String jwtToken = authService.register(request).getAccessToken();
     HttpHeaders headers = new HttpHeaders();
     headers.add("Authorization", "Access-Token " + jwtToken);
     return new ResponseEntity<>(headers, HttpStatus.CREATED);
-  }
-
-  @PostMapping("/register-admin")
-  public ResponseEntity<AuthenticationResponse> registerAdmin(
-      @RequestBody RegisterRequest request) {
-    String jwtToken = authenticationService.registerAdmin(request).getAccessToken();
-    HttpHeaders headers = new HttpHeaders();
-    headers.add("Authorization", "Access-Token " + jwtToken);
-    return new ResponseEntity<>(headers, HttpStatus.CREATED);
-  }
-
-  @PutMapping("/update-role/{username}")
-  public ResponseEntity<AuthenticationResponse> updateRole(@PathVariable String username,
-                                                           @RequestBody
-                                                           UpdateRoleRequest updateRequest) {
-    return ResponseEntity.ok(authenticationService.updateRole(username, updateRequest));
-  }
-
-  @PutMapping("/update-data/{username}")
-  public ResponseEntity<AuthenticationResponse> updateData(@PathVariable String username,
-                                                           @RequestBody
-                                                           UpdateDataRequest updateRequest) {
-    return ResponseEntity.ok(authenticationService.updateUserData(username, updateRequest));
-  }
-
-  @DeleteMapping("/delete/{username}")
-  public void delete(@PathVariable String username) {
-    authenticationService.deleteUser(username);
   }
 
   @PostMapping("/login")
-  public ResponseEntity<AuthenticationResponse> authenticate(
-      @RequestBody AuthenticationRequest request) {
-    AuthenticationResponse authResponse = authenticationService.authenticate(request);
+  public ResponseEntity<AuthResponse> authenticate(
+      @RequestBody AuthRequest request) {
+    AuthResponse authResponse = authService.authenticate(request);
     String jwtToken = authResponse.getAccessToken();
     String refreshToken = authResponse.getRefreshToken();
     HttpHeaders headers = new HttpHeaders();
@@ -75,47 +42,5 @@ public class AuthenticationController {
         "Refresh-Token " + refreshToken
     );
     return new ResponseEntity<>(headers, HttpStatus.OK);
-  }
-
-  @PostMapping("/refresh-token")
-  public ResponseEntity<String> refreshToken(
-      HttpServletRequest request
-  ) {
-    AuthenticationResponse authResponse = authenticationService.refreshToken(
-        request
-    );
-
-    HttpHeaders headers = new HttpHeaders();
-    headers.set(
-        "Authorization",
-        "Access-Token " + authResponse.getAccessToken()
-    );
-    headers.add(
-        "Authorization",
-        "Refresh-Token "
-            + authResponse.getRefreshToken()
-    );
-
-    return ResponseEntity.ok()
-        .headers(headers)
-        .build();
-  }
-
-  @PutMapping("/change-own-password")
-  public ResponseEntity<?> changeOwnPassword(
-      @RequestBody ChangePasswordRequest request,
-      Principal connectedUser
-  ) {
-    authenticationService.changeOwnPassword(request, connectedUser);
-    return ResponseEntity.ok().build();
-  }
-
-  @PutMapping("/change-password/{username}")
-  public ResponseEntity<?> changePassword(@PathVariable String username,
-                                          @RequestBody ChangePasswordRequest request,
-                                          Principal connectedUser
-  ) {
-    authenticationService.changePassword(request, connectedUser, username);
-    return ResponseEntity.ok().build();
   }
 }
--- src/main/java/ch/noseryoung/gg/user/UserRepository.java
+++ src/main/java/ch/noseryoung/gg/user/UserRepository.java
@@ -1,7 +1,12 @@
 package ch.noseryoung.gg.user;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
     boolean existsByUsername(String username);
+
+  Optional<Object> findByEmail(String email);
+
+  Optional<Object> findByUsername(String username);
 }
